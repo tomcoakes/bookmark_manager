b414eb5e1665e76ffb6e50d9aeeec0d5fe8d9972
Tom Coakes <tomcoakes@gmail.com>
2015-02-25 12:32:01 +0000

    Links have tags and tags have links. Many to many joined database

--- lib/link.rb
+++ lib/link.rb
@@ -5,4 +5,6 @@ class Link
   property :title,  String
   property :url,    String
 
+  has n, :tags, :through => Resource
+
 end
--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/tag'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
@@ -20,7 +21,8 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     url = params["url"]
     title = params["title"]
-    Link.create(:url => url, :title => title)
+    tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(text: tag) }
+    Link.create(url: url, title: title, tags: tags)
     redirect to('/')
   end
 
