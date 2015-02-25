fd0a4ace532f7fd6e474a122d9702a5fa3f160d6
Tom Coakes <tomcoakes@gmail.com>
2015-02-25 09:25:07 +0000

    Homepage lists links and can add new ones

--- server.rb
+++ server.rb
@@ -13,7 +13,15 @@ DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Hello BookmarkManager!'
+    @links = Link.all
+    erb :index
+  end
+
+  post '/links' do
+    url = params["url"]
+    title = params["title"]
+    Link.create(:url => url, :title => title)
+    redirect to('/')
   end
 
   run! if app_file == $0
