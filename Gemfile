99ac72c4dae48c08f6d2b11d2fbbc513d4f841fc
Tom Coakes <tomcoakes@gmail.com>
2015-02-25 22:09:20 +0000

    User sign up now works. Tests passing. Datamapper extracted, but helper file isn't because I was having issues.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'bcrypt-ruby'
 
 gem 'sinatra'
 gem 'rspec'
--- server.rb
+++ server.rb
@@ -1,18 +1,15 @@
 require 'sinatra/base'
 require 'data_mapper'
-
-env = ENV['RACK_ENV'] || 'development'
-
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
 require './lib/link'
 require './lib/tag'
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
+require './lib/user'
+require_relative 'data_mapper_setup'
 
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret , 'super_secret'
+
   get '/' do
     @links = Link.all
     erb :index
@@ -30,6 +27,22 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(:text => params[:text])
     @links = tag ? tag.links : []
     erb :index
+  end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
   end
 
   run! if app_file == $0
