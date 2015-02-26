4e9e856703919e13ea83a821ab208c73d95a7040
Tom Coakes <tomcoakes@gmail.com>
2015-02-26 17:07:08 +0000

    Sign out functionality now included

--- server.rb
+++ server.rb
@@ -15,6 +15,7 @@ class BookmarkManager < Sinatra::Base
   enable :sessions
   set :session_secret , 'super_secret'
   use Rack::Flash
+  use Rack::MethodOverride
 
   get '/' do
     @links = Link.all
@@ -67,6 +68,12 @@ class BookmarkManager < Sinatra::Base
       flash[:errors] = ["The email or password is incorrect"]
       erb :"sessions/new"
     end
+  end
+
+  delete '/sessions' do
+    session[:user_id] = nil
+    flash[:notice] = ["Good bye!"]
+    erb :"sessions/new"
   end
 
   run! if app_file == $0
