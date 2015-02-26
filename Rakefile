7a35015f9d61327630a67a2b3b10d98b5c4a5a49
Tom Coakes <tomcoakes@gmail.com>
2015-02-26 16:20:26 +0000

    Sign in functionality built

--- Rakefile
+++ Rakefile
@@ -1,3 +1,4 @@
+require 'stringio'
 require 'data_mapper'
 require './data_mapper_setup'
 
--- server.rb
+++ server.rb
@@ -44,13 +44,28 @@ class BookmarkManager < Sinatra::Base
     @user = User.new(email: params[:email],
                     password: params[:password],
                     password_confirmation: params[:password_confirmation] )
-
     if @user.save
       session[:user_id] = @user.id
       redirect to('/')
     else
       flash.now[:errors] = @user.errors.full_messages
       erb :"users/new"
+    end
+  end
+
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+      flash[:errors] = ["The email or password is incorrect"]
+      erb :"sessions/new"
     end
   end
 
