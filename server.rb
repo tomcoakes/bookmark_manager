f423bdb5fa2c8fae6435f1b3ff2eab2c154c27e8
Tom Coakes <tomcoakes@gmail.com>
2015-02-26 16:41:50 +0000

    Refactored user_management specs by putting helper methods in a helper module

--- server.rb
+++ server.rb
@@ -71,3 +71,4 @@ class BookmarkManager < Sinatra::Base
 
   run! if app_file == $0
 end
+
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require './helpers/session'
+
+include SessionHelpers
 
 feature "User signs up" do
 
@@ -20,13 +23,45 @@ feature "User signs up" do
     expect(page).to have_content("This email is already taken")
   end
 
-  def sign_up(email = "alice@example.com", password = "oranges!", password_confirmation = "oranges!")
-    visit '/users/new'
-    expect(page.status_code).to eq(200)
-    fill_in :email, :with => email
-    fill_in :password, :with => password
-    fill_in :password_confirmation, :with => password_confirmation
-    click_button "Sign up"
+end
+
+feature "User signs in" do
+
+  before(:each) do
+    User.create(email: "test@test.com",
+                password: "test",
+                password_confirmation: "test")
+  end
+
+  scenario "with correct credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in("test@test.com", "test")
+    expect(page).to have_content("Welcome, test@test.com")
+  end
+
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in("test@test.com", "wrong")
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+end
+
+feature "User signs out" do
+  
+  before(:each) do
+    User.create(email: "test@test.com",
+                password: "test",
+                password_confirmation: "test")
+  end
+
+  scenario "while being signed in" do
+    sign_in("test@test.com", "test")
+    click_button("Sign out")
+    expect(page).to have_content("Good bye!")
+    expect(page).not_to have_content("Welcome, test@test.com")
   end
 
 end
